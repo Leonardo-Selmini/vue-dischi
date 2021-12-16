810f7a072be522751c6cc369c0c8df0e5024f55d
Leonardo.Selmini <selminileonardo@gmail.com>
2021-12-16 16:44:33 +0100

    header component

---
+++ src/components/macro/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <img src="../../assets/logo.png" alt="spotify logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/variables.scss';
+
+  header {
+    height: 10vh;
+    background-color: $darkPrimary;
+
+    img {
+      height: 80%;
+    }
+  }
+</style>
